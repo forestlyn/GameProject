47d6821140f5ecc83066267e89bd74970e4a61a7
forestlyn <81517120+forestlyn@users.noreply.github.com>
2022-07-28 12:18:37 +0800

    none

--- 迷失岛/Test/Assets/Scripts/Mouse/MouseState.cs
+++
@@ -1,4 +0,0 @@
-public enum MouseState
-{
-    Default, Holding
-}
--- 迷失岛/Test/Assets/跟随视频制作/Scripts/Dialogue/Data/DialogueData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName ="DialogueData",menuName =("dialog/New dialogueData"))]
-public class DialogueData : ScriptableObject
-{
-    public List<string> dialogueList;
-}
